7c117c2fb10a815e936216f522c720e5297aae28
Alexis Zugasti <alexiszugasti@gmail.com>
2024-01-08 04:16:47 -0600

    fix

--- src/components/ui/themeButton.tsx
+++ src/components/ui/themeButton.tsx
@@ -24,16 +24,14 @@ export function ThemeButton() {
   return (
     <div>
       <Button variant="outline" size="icon" onClick={toggleTheme}>
-        <Sun
-          className={`h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all ${
-            isDarkTheme ? "dark:-rotate-90 dark:scale-0" : ""
-          }`}
-        />
-        <Moon
-          className={`absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all ${
-            isDarkTheme ? "dark:rotate-0 dark:scale-100" : ""
-          }`}
-        />
+        {isDarkTheme ? (
+          <Moon
+            className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:-rotate-0 dark:scale-100 " />
+        ) : (
+          <Sun
+            className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+          />
+        )}
         <span className="sr-only">Toggle theme</span>
       </Button>
     </div>
